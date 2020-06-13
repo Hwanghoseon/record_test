d075734f1419a0e7e467d288872e3e58de962f09
Hwanghoseon <hosun278@naver.com>
2020-06-13 16:15:35 +0900

    녹음 파일 생성, 녹을 목록 화면 추가

--- app/src/main/java/com/example/android_newsky/navigation/RecordActivity.java
+++ app/src/main/java/com/example/android_newsky/navigation/RecordActivity.java
@@ -2,13 +2,16 @@ package com.example.android_newsky.navigation;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.ContactsContract;
@@ -23,8 +26,14 @@ import android.content.pm.PackageManager;
 import com.example.android_newsky.R;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class RecordActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
     private MediaPlayer player;
@@ -32,31 +41,52 @@ public class RecordActivity extends AppCompatActivity {
 
     private final File file = Environment.getExternalStorageDirectory();
     //갤럭시 S4기준으로 /storage/emulated/0/의 경로를 갖고 시작한다.
-    private final String PATH = file.getAbsolutePath() + "/" + "recoder.mp3";
+    String dataTimeNow = LocalDateTime.now().toString();
+    private final String PATH = file.getAbsolutePath() + "/" + dataTimeNow + "_recorder.mp3";
+    private String saveFilePath = null;
     private int position;
+    String dirPath = file.getAbsoluteFile() + "/Recorder";
+    File fileFolder = new File(dirPath);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+
+        if(!fileFolder.exists())
+            fileFolder.mkdirs();
+
+        //saveFilePath = fileFolder + "/Recorder" + LocalDateTime.now().toString()+ "_recorder.mp3";
     }
 
     public void onButton1Clicked(View v) {
         Log.d("RecordActivity", "시작 버튼 클릭됨!");
 
+        FileInputStream fis = null;
         try {
-            KillPlayer();
+            try {
+                KillPlayer();
 
-            player = new MediaPlayer();
-            player.setDataSource(PATH);
-            player.prepare();
-            player.start();
+                player = new MediaPlayer();
 
-        } catch (IOException e) {
+                fis = new FileInputStream(saveFilePath);
+                FileDescriptor fd = fis.getFD();
+                player.setDataSource(fd);
+
+                player.prepare();
+                player.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Toast.makeText(getApplicationContext(), "재생을 시작합니다!", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "재생할 파일이 존재하지 않습니다!", Toast.LENGTH_LONG).show();
+
             e.printStackTrace();
         }
-
-        Toast.makeText(getApplicationContext(), "재생을 시작합니다!", Toast.LENGTH_LONG).show();
     }
 
     private void KillPlayer() {
@@ -78,13 +108,16 @@ public class RecordActivity extends AppCompatActivity {
     }
 
     public void onButton3Clicked(View v) {
-        Log.d("RecordActivity", "재시작 버튼 클릭됨!");
+        Log.d("RecordActivity", "목록 버튼 클릭됨!");
 
-        if (player != null && !player.isPlaying()) {
-            player.start();
-            player.seekTo(position);
-        }
-        Toast.makeText(getApplicationContext(), "재생을 재시작합니다!", Toast.LENGTH_LONG).show();
+//        if (player != null && !player.isPlaying()) {
+//            player.start();
+//            player.seekTo(position);
+//        }
+//        Toast.makeText(getApplicationContext(), "재생을 재시작합니다!", Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(getApplicationContext(), RecorderPlayerActivity.class);
+        startActivity(intent);
     }
 
     public void onButton4Clicked(View v) {
@@ -124,7 +157,9 @@ public class RecordActivity extends AppCompatActivity {
 
             //File file= Environment.getExternalStorageDirectory();
 //갤럭시 S4기준으로 /storage/emulated/0/의 경로를 갖고 시작한다.
-            //String path=file.getAbsolutePath()+"/"+"recoder.mp3";
+            //String path=file.getAbsolutePath()+"/"+"recorder.mp3";
+
+            saveFilePath = fileFolder + "/Recorder" + LocalDateTime.now().toString()+ "_recorder.mp3";
 
             recorder = new MediaRecorder();
 
@@ -132,11 +167,11 @@ public class RecordActivity extends AppCompatActivity {
                 recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                 recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
                 recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-                recorder.setOutputFile(PATH);
+                recorder.setOutputFile(saveFilePath);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            System.out.println(PATH);
+            //System.out.println(PATH);
             recorder.prepare();
             recorder.start();
 
@@ -159,7 +194,6 @@ public class RecordActivity extends AppCompatActivity {
             values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000);
             values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp4");
             values.put(MediaStore.Audio.Media.DATA, PATH);
-
             Uri audioUri = getContentResolver().insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);*/
 
             recorder.release();
